5989ab1382db9a792849e82e36f166f15557f1e7
BABBITTRON\owenh <owen.harbert2@gmail.com>
2026-02-19 23:46:59 +0200

    add: generic enum event add: generic enum variable

---
+++ EventSystem/Events/EnumEvent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.SOAP.EventSystem.Events
+{
+    [CreateAssetMenu(menuName = "Events/Enum Event")]
+    public class EnumEvent<T> : GameEvent<T> where T : Enum { }
+}
---
+++ Variables/EnumVariable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.SOAP.Variables
+{
+    [CreateAssetMenu(fileName = "EnumVariable", menuName = "Variables/EnumVariable")]
+    public class EnumVariable<T> : ScriptableVariable<T> where T : Enum
+    {
+        protected override bool EqualityComparer(T a, T b)
+        {
+            return a.Equals(b);
+        }
+    }
+}
