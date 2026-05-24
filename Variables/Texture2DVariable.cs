95ea1ff6e7b1246698e35ad8ad951ce8b18474c9
BABBITTRON\owenh <owen.harbert2@gmail.com>
2026-05-24 23:14:04 +0200

    add: Texture2D Scriptable Variable

---
+++ Variables/Texture2DVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SOAP.Variables
+{
+    [CreateAssetMenu(fileName = "Texture2DVariable", menuName = "Variables/Texture2DVariable")]
+    public class Texture2DVariable : ScriptableVariable<Texture2D>
+    {
+        protected override bool EqualityComparer(Texture2D a, Texture2D b)
+        {
+            return a == b;
+        }
+    }
+}
