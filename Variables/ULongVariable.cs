e39c6a82590696a755c145a31dc52835f24614b9
BABBITTRON\owenh <owen.harbert2@gmail.com>
2026-06-04 23:26:58 +0200

    add: ULong Variable

---
+++ Variables/ULongVariable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SOAP.Variables
+{
+    [CreateAssetMenu(fileName = "ULongVariable", menuName = "Variables/ULongVariable")]
+    public class ULongVariable : ScriptableVariable<ulong>
+    {
+        protected override bool EqualityComparer(ulong a, ulong b)
+        {
+            return string.Equals(a, b);
+        }
+    }
+}
