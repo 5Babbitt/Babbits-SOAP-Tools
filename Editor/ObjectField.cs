ea8ea9c94a8f1fd63ff416ca9770e673b958d7fe
BABBITTRON\owenh <owen.harbert2@gmail.com>
2026-06-20 01:41:47 +0200

    fix: update VariableDrawers

---
+++ Editor/ObjectField.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace SOAP.Editor
+{
+    public class ObjectField<T> : BaseField<T> where T : UnityEngine.Object
+    {
+        private readonly ObjectField objectField;
+
+        public ObjectField() : this(null) { }
+
+        public ObjectField(string label) : base(label, new ObjectField())
+        {
+            objectField = this.Q<ObjectField>();
+            objectField.objectType = typeof(T);
+            objectField.RegisterValueChangedCallback(evt => value = evt.newValue as T);
+        }
+
+        public override T value
+        {
+            get => base.value;
+            set { base.value = value; objectField.SetValueWithoutNotify(value); }
+        }
+    }
+}
--- Editor/ScriptableVariableDrawers.cs
+++ Editor/ScriptableVariableDrawers.cs
@@ -6,26 +6,31 @@ using UnityEngine.UIElements;
 namespace SOAP.Editor
 {
     // Primitive Types
+    [CustomPropertyDrawer(typeof(Variables.BoolVariable))]
+    public class BoolVariableDrawer : GenericVariableDrawer<Variables.BoolVariable, bool, Toggle> { }
+    
     [CustomPropertyDrawer(typeof(Variables.FloatVariable))]
     public class FloatVariableDrawer : GenericVariableDrawer<Variables.FloatVariable, float, FloatField> { }
     
     [CustomPropertyDrawer(typeof(Variables.IntVariable))]
     public class IntVariableDrawer : GenericVariableDrawer<Variables.IntVariable, int, IntegerField> { }
     
-    [CustomPropertyDrawer(typeof(Variables.BoolVariable))]
-    public class BoolVariableDrawer : GenericVariableDrawer<Variables.BoolVariable, bool, Toggle> { }
-    
     [CustomPropertyDrawer(typeof(Variables.StringVariable))]
     public class StringVariableDrawer : GenericVariableDrawer<Variables.StringVariable, string, TextField> { }
     
+    [CustomPropertyDrawer(typeof(Variables.ULongVariable))]
+    public class ULongVariableDrawer : GenericVariableDrawer<Variables.ULongVariable, ulong, UnsignedLongField> { }
+    
     // Unity Types
+    [CustomPropertyDrawer(typeof(Variables.ColourVariable))]
+    public class ColourVariableDrawer : GenericVariableDrawer<Variables.ColourVariable, Color, ColorField> { }
+    
     [CustomPropertyDrawer(typeof(Variables.Vector2Variable))]
     public class Vector2VariableDrawer : GenericVariableDrawer<Variables.Vector2Variable, Vector2, Vector2Field> { }
     
     [CustomPropertyDrawer(typeof(Variables.Vector3Variable))]
     public class Vector3VariableDrawer : GenericVariableDrawer<Variables.Vector3Variable, Vector3, Vector3Field> { }
     
-    [CustomPropertyDrawer(typeof(Variables.ColourVariable))]
-    public class ColourVariableDrawer : GenericVariableDrawer<Variables.ColourVariable, Color, ColorField> { }
-    
+    [CustomPropertyDrawer(typeof(Variables.Texture2DVariable))]
+    public class Texture2DVariableDrawer : GenericVariableDrawer<Variables.Texture2DVariable, Texture2D, ObjectField<Texture2D>> { }
 }
